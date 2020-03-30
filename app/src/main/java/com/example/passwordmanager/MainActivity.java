1b8d12415cdbfa494a6a47f62f38705087e5e41f
unknown <mhdbnd@mail.umkc.edu>
2020-03-29 19:28:48 -0500

    login functionality

--- app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private int counter = 5;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.username);
+        Password = (EditText)findViewById(R.id.password);
+        Login = (Button)findViewById(R.id.button);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate (String Username, String UserPassword){
+        if ((Username.equals("admin")) && (UserPassword.equals("password"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter --;
+            if (counter == 0){
+                Login.setEnabled(false);
+
+            }
+            new CountDownTimer(5000,1000){
+                @Override
+                public void onTick(long millisUntilFinished) {
+                }
+
+                @Override
+                public void onFinish() {
+                    Login.setEnabled(true);
+                    counter = 5;
+                }
+            }.start();
+        }
     }
 }
